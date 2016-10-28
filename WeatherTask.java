756f0ca727433791ff43544ea36afd607423cdc8
cwang16 <cwang16@kent.edu>
2016-10-28 11:04:12 -0500

    Open "WeatherTask.java" --> Copy the code around comment.
     Since I haven't set my file system as Conor's computer did, I debugged it in my pervious app, the json parser works fine, which shows the percipi :
    2016/10/25 0.3
    2016/10/26 0.49
    Please let me know if I can do other modification

---
+++ WeatherTask.java
@@ -0,0 +1,80 @@
+package com.example.myfirst.myapplication;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class WeatherTask extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        try {
+
+
+            URL url = new URL(params[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+            //**************************************************COPY FROM HERE ********************************************
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+            if (params[0].contains("history")) {
+                String finalJson = buffer.toString();
+                JSONObject parentObject = new JSONObject(finalJson);
+                JSONObject historyObject = parentObject.getJSONObject("history");
+                JSONArray dailysummaryArray = historyObject.getJSONArray("dailysummary");
+                JSONObject Object = dailysummaryArray.getJSONObject(0);
+                String precipitation = Object.getString("precipi");
+                return precipitation;
+
+
+            }
+            else if (params[0].contains("conditions")) {
+                String finalJson = buffer.toString();
+                JSONObject parentObject = new JSONObject(finalJson);
+                JSONObject current_observation = parentObject.getJSONObject("current_observation");
+                String temperature = current_observation.getString("temp_f");
+                return temperature;
+            }
+            //**************************************************COPY END ***************************************************
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                reader.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+    }
+}
